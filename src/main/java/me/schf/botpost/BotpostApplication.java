9b51b1d0ff48d695fb9a435d804027398242b2a9
Schaefer <michael.schaefer@startribune.com>
2025-06-19 09:10:11 -0500

    init

---
+++ src/main/java/me/schf/botpost/BotpostApplication.java
@@ -0,0 +1,13 @@
+package me.schf.botpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotpostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotpostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/schf/botpost/BotpostApplicationTests.java
@@ -0,0 +1,13 @@
+package me.schf.botpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotpostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
